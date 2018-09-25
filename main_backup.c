a557f5f59ecabf807ec3c9d838a9fdacb1801ead
Cahue BELTRAO <cbeltrao@e2r9p9.42.fr>
2018-09-25 13:28:31 +0200

    Testing bresenham reduction

--- main_backup.c
+++ main_backup.c
@@ -6,15 +6,13 @@
 /*   By: cbeltrao <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/08/01 11:54:03 by cbeltrao          #+#    #+#             */
-/*   Updated: 2018/09/25 13:28:18 by cbeltrao         ###   ########.fr       */
+/*   Updated: 2018/09/25 13:05:22 by cbeltrao         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include "mlx.h"
 #include "fdf.h"
-#include <math.h>
-#include <stdlib.h>
 #include <unistd.h>
 
 void	ft_putchar(char c)
@@ -56,80 +54,53 @@ void	ft_swap(int *a, int *b)
 
 /* Bresenhams Algorithm to draw a line */
 void	draw_line_da_x(void *mlx_ptr, void *win_ptr,
-	   	t_2dpoint initial_p, t_2dpoint final_p)
+	   	t_2dpoint *initial_p, t_2dpoint *final_p)
 {
+	float e;
 	int x;
+	int delta_x;
 	int y;
-	int dx;
-	int dy;
-	int xincr, yincr;
-	int err;
-	int i;
+	int delta_y;
 
-	dx = abs(final_p.x - initial_p.x);
-	dy = abs(final_p.y - initial_p.y);
+	delta_x = final_p->x - initial_p->x;
+	delta_y = final_p->y - initial_p->y;
+	
+	if (initial_p->x > final_p->x)
+		ft_swap(&initial_p->x, &final_p->x);
+	if (initial_p->y > final_p->y)
+		ft_swap(&initial_p->y, &final_p->y);
+	printf("asdasd %d %d", initial_p->x, final_p->x);
+	fflush(stdout);
+	x = initial_p->x;
+	y = initial_p->y;
 
-	xincr = (initial_p.x < final_p.x ? 1 : -1);	
-	/* if (initial_p.x < final_p.x)
-		xincr = 1;
-	else
-		xincr = -1;
-	*/
-	yincr = (initial_p.y < final_p.y ? 1 : -1);
-	/* if (initial_p.y < final_p.y)
-		yincr = 1;
-	else
-		yincr = -1;
-	*/
-	x = initial_p.x;
-	y = initial_p.y;
-	if (dx > dy)
+	delta_x = (delta_x == 0) ? 1 : delta_x;
+	delta_y = (delta_y == 0) ? 1 : delta_y;
+	e = (float)(delta_x / delta_y) - 1.0;
+
+	while(x < final_p->x - 1 || y != final_p->y > y - 1)
 	{
-		err = dx / 2;
-		i = 0;
-		while (i < dx)
+		mlx_pixel_put(mlx_ptr, win_ptr, x, y, 0XFF00FF);
+		if ( e >= 0)
 		{
-			x += xincr;
-			err += dy;
-			if (err > dx)
-			{
-				err -= dx;
-				y += yincr;
-			}	
-			mlx_pixel_put(mlx_ptr, win_ptr, x, y, 0XFF00FF);
-			i++;
+			y += 1;
+			e -= 1.0;
 		}
+		x += 1;
+		e += (float)delta_x / delta_y;
 	}
-	else
-	{
-		err = dy / 2;
-		i = 0;
-		while (i < dy)
-		{
-			y += yincr;
-			err += dx;
-			if (err > dy)
-			{
-				err -= dy;
-				x += xincr;
-			}
-			mlx_pixel_put(mlx_ptr, win_ptr, x, y, 0XFF00FF);
-			i++;
-		}
-	}
-	mlx_pixel_put(mlx_ptr, win_ptr, x, y, 0XFF00FF);
 	mlx_pixel_put(mlx_ptr, win_ptr, x, y, 0XFF00FF);
 }
 
-int		draw_line(void *mlx_ptr, void *win_ptr, t_2dpoint initial_p,
-	   	t_2dpoint final_p)
+int		draw_line(void *mlx_ptr, void *win_ptr, t_2dpoint *initial_p,
+	   	t_2dpoint *final_p)
 {
 	int delta_x;
 	int delta_y;
 	int dominant_axis;
 
-	delta_x = final_p.x - initial_p.x;
-	delta_y = final_p.y - initial_p.y;
+	delta_x = final_p->x - initial_p->x;
+	delta_y = final_p->y - initial_p->y;
 	dominant_axis = set_da(delta_x, delta_y);
 	/* if (dominant_axis == 'x') */
 	draw_line_da_x(mlx_ptr, win_ptr, initial_p, final_p);
@@ -144,8 +115,8 @@ int main()
 	t_2dpoint	final_point;
 
 	/* Set point variables */
-	set_point(&initial_point, 500, WIN_HEIGHT/2);
-	set_point(&final_point, 800, 1);
+	set_point(&initial_point, 500, WIN_HEIGHT / 2);
+	set_point(&final_point, 300, WIN_HEIGHT);
 
 	/* Initialize connection with graphical server */
 	mlx_ptr = mlx_init();
@@ -154,7 +125,7 @@ int main()
 	win_ptr = mlx_new_window(mlx_ptr, WIN_WIDTH, WIN_HEIGHT, "FdF");
 
 	/* Initialize drawing between 2 points */
-	draw_line(mlx_ptr, win_ptr, initial_point, final_point);
+	draw_line(mlx_ptr, win_ptr, &initial_point, &final_point);
 
 	/* Same as pixel_put, but puts string instead of single pixel */
 	 mlx_string_put(mlx_ptr, win_ptr, 300,300, 0XFF00FF, "HOLA SIMONE");
