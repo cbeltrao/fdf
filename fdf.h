048f14917bce8b4eb946d9eae9771a426f0de86e
Cahue BELTRAO <cbeltrao@e1r5p10.42.fr>
2018-10-28 00:02:59 +0200

    Funcionalities completed - Todo: organize makefile & .c files

--- fdf.h
+++ fdf.h
@@ -6,39 +6,74 @@
 /*   By: cbeltrao <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/08/06 18:02:08 by cbeltrao          #+#    #+#             */
-/*   Updated: 2018/10/27 13:42:18 by cbeltrao         ###   ########.fr       */
+/*   Updated: 2018/10/27 23:54:20 by cbeltrao         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FDF_H
 # define FDF_H
+
 # define HEIGHT 1200
 # define WIDTH 1800
-# define I_X ((900) - map->grid[i][0])
-# define I_Y ((0) - map->grid[i][0] + (map->k * i))
 # define SCALE(x) (2 * x)
-
+# define REFRESH 100
+# define INITIAL 101
+# define PARALLEL 1
+# define ISOMETRIC 2
+# define ZOOM_IN 3
+# define ZOOM_OUT 4
+# define INCREASE_HEIGHT 5
+# define DECREASE_HEIGHT 6
+# define MOVE_TO_RIGHT 7
+# define MOVE_TO_LEFT 8
+# define MOVE_UP 9
+# define MOVE_DOWN 10
 # define INVAL_MEM_ERROR -1
 # define INVAL_MAP_ERROR -2
-# define LINE_LEN_ERROR -3;
+# define LINE_LEN_ERROR -3
+# define SUCCESS 0
+# define E 14
+# define Q 12
+# define R 15
+# define F 3
+# define D 2
+# define A 0
+# define S 1
+# define W 13
+# define Z 6
+# define X 7
+# define ESC 53
+# define EXIT -1
+# define MENU_COLOR 0XFF6D8A
 
 typedef struct	s_2dpoint
 {
-	int 		x;
-	int 		y;
+	int			x;
+	int			y;
+	int			z;
 	int			iso_x;
 	int			iso_y;
-	int			z;
 }				t_2dpoint;
 
 typedef	struct	s_img
 {
 	void			*img_ptr;
-	unsigned int	*img_ui;
+	unsigned int	*pixel_pos;
 	int				size_l;
 	int				bpp;
 	int				endian;
 }				t_img;
+
+typedef struct	s_bresen
+{
+	int			dx;
+	int			dy;
+	int			x;
+	int			y;
+	int			xincr;
+	int			yincr;
+	int			err;
+}				t_bresen;
 
 typedef	struct	s_map
 {
@@ -47,17 +82,19 @@ typedef	struct	s_map
 	int			move_y;
 	int			len;
 	int			dep;
-	float		k; // scale
-	int			z_h; // height exclusive scale
+	int			cam;
+	float		k;
+	int			height_k;
 	t_2dpoint	**p;
 }				t_map;
 
 typedef	struct	s_mlx
 {
-	void 		*mlx_ptr;
+	void		*mlx_ptr;
 	void		*win_ptr;
 	t_img		img;
 	t_map		*map;
+	t_bresen	bres;
 }				t_mlx;
 
 #endif
--- main.c
+++ main.c
@@ -5,8 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: cbeltrao <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/08/01 11:54:03 by cbeltrao          #+#    #+#             */
-/*   Updated: 2018/10/27 16:27:00 by cbeltrao         ###   ########.fr       */
+/*   Created: 2018/10/27 22:59:53 by cbeltrao          #+#    #+#             */
+/*   Updated: 2018/10/28 00:01:50 by cbeltrao         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,71 +20,49 @@
 #include "gnl/get_next_line.h"
 #include "libft/includes/libft.h"
 #include <unistd.h>
-#define DEPTH_OF(x) (sizeof(x)/sizeof(x[0])
 
-int		image_refresh(t_mlx *mlx, char *mode);
+int				fdf_refresh(t_mlx *mlx, int mode);
 
-int			deal_key(int key, t_mlx *mlx)
+int				deal_key(int key, t_mlx *mlx)
 {
-	ft_putchar(key);
-	printf("ENTROU%dSAIU\n", key);
-	fflush(stdout);
-	if(key == 12)
-		image_refresh(mlx, "plus_scale");
-	else if(key == 13)
-		image_refresh(mlx, "minus_scale");
-	else if(key == 14)
-		image_refresh(mlx, "plus_z_height");
-	else if (key == 15)
-		image_refresh(mlx, "minus_z_height");
-	else if (key == 0)
-		image_refresh(mlx, "add_move_x");
-	else if (key == 1)
-		image_refresh(mlx, "sub_move_x");
-	else if (key == 2)
-		image_refresh(mlx, "add_move_y");
-	else if (key == 3)
-		image_refresh(mlx, "sub_move_y");
-	else if (key == 53)
-	{
-		printf("ESC PRESSED\n");
-		fflush(stdout);
-		return (1);
-	}
-	return (0);
+	if (key == E)
+		fdf_refresh(mlx, ZOOM_IN);
+	else if (key == Q)
+		fdf_refresh(mlx, ZOOM_OUT);
+	else if (key == R)
+		fdf_refresh(mlx, INCREASE_HEIGHT);
+	else if (key == F)
+		fdf_refresh(mlx, DECREASE_HEIGHT);
+	else if (key == D)
+		fdf_refresh(mlx, MOVE_TO_RIGHT);
+	else if (key == A)
+		fdf_refresh(mlx, MOVE_TO_LEFT);
+	else if (key == S)
+		fdf_refresh(mlx, MOVE_UP);
+	else if (key == W)
+		fdf_refresh(mlx, MOVE_DOWN);
+	else if (key == Z)
+		fdf_refresh(mlx, ISOMETRIC);
+	else if (key == X)
+		fdf_refresh(mlx, PARALLEL);
+	else if (key == ESC)
+		fdf_refresh(mlx, EXIT);
+	return (SUCCESS);
 }
 
-void		iso(int *x, int *y, int z)
-{
-	int	previous_x;
-	int previous_y;
-
-	previous_x = *x;
-	previous_y = *y;
-	*x = (previous_x - previous_y) * cos(0.523599);
-	*y = -z + (previous_x + previous_y) * sin(0.523599);
-}
-
-t_2dpoint	set_point(int x, int y, int z)
+t_2dpoint		set_point(int x, int y, int z)
 {
 	t_2dpoint temp;
 
 	temp.x = x;
 	temp.y = y;
 	temp.z = z;
+	temp.iso_x = (x - y) * cos(0.523599);
+	temp.iso_y = -z + (x + y) * sin(0.523599);
 	return (temp);
 }
 
-void		ft_swap(int *a, int *b)
-{
-	int tmp;
-
-	tmp = *a;
-	*a = *b;
-	*b = tmp;
-}
-
-void		fill_pixel(unsigned int *img, int x, int y, int z)
+void			fill_pixel(unsigned int *img, int x, int y, int z)
 {
 	if (x < WIDTH && y < HEIGHT && x >= 0 && y >= 0)
 	{
@@ -107,117 +85,110 @@ void		fill_pixel(unsigned int *img, int x, int y, int z)
 	}
 }
 
-void		draw_line_da_y(t_mlx *mlx, t_2dpoint p1, t_2dpoint p2)
+void			draw_line_da_y(t_mlx *mlx, t_2dpoint p1, t_2dpoint p2)
 {
-	int x;
-	int y;
-	int dx;
-	int dy;
-	int xincr;
-	int	yincr;
-	int err;
 	int i;
 
-	dx = abs(p2.x - p1.x);
-	dy = abs(p1.y - p2.y);
+	mlx->bres.dx = abs(p2.x - p1.x);
+	mlx->bres.dy = abs(p2.y - p1.y);
+	mlx->bres.xincr = (p1.x < p2.x ? 1 : -1);
+	mlx->bres.yincr = (p1.y < p2.y ? 1 : -1);
+	mlx->bres.err = mlx->bres.dy / 2;
+	mlx->bres.x = p1.x;
+	mlx->bres.y = p1.y;
 	i = 0;
-	xincr = (p1.x < p2.x ? 1 : -1);
-	yincr = (p1.y < p2.y ? 1 : -1);
-	x = p1.x;
-	y = p1.y;
-	err = dy / 2;
-	while (i++ < dy)
+	while (i++ < mlx->bres.dy)
 	{
-		y += yincr;
-		err += dx;
-		if (err > dy)
+		mlx->bres.y += mlx->bres.yincr;
+		mlx->bres.err += mlx->bres.dx;
+		if (mlx->bres.err > mlx->bres.dy)
 		{
-			err -= dy;
-			x += xincr;
+			mlx->bres.err -= mlx->bres.dy;
+			mlx->bres.x += mlx->bres.xincr;
 		}
-		fill_pixel(mlx->img.img_ui, x, y, p1.z);
+		fill_pixel(mlx->img.pixel_pos, mlx->bres.x, mlx->bres.y, p1.z);
 	}
-	fill_pixel(mlx->img.img_ui, x, y, p1.z);
+	fill_pixel(mlx->img.pixel_pos, mlx->bres.x, mlx->bres.y, p1.z);
 }
 
-void		draw_line_da_x(t_mlx *mlx, t_2dpoint p1, t_2dpoint p2)
+void			draw_line_da_x(t_mlx *mlx, t_2dpoint p1, t_2dpoint p2)
 {
-	int x;
-	int y;
-	int dx;
-	int dy;
-	int xincr;
-	int yincr;
-	int err;
 	int i;
 
-	dx = abs(p2.x - p1.x);
-	dy = abs(p2.y - p1.y);
+	mlx->bres.dx = abs(p2.x - p1.x);
+	mlx->bres.dy = abs(p2.y - p1.y);
+	mlx->bres.xincr = (p1.x < p2.x ? 1 : -1);
+	mlx->bres.yincr = (p1.y < p2.y ? 1 : -1);
+	mlx->bres.err = mlx->bres.dx / 2;
+	mlx->bres.x = p1.x;
+	mlx->bres.y = p1.y;
 	i = 0;
-	xincr = (p1.x < p2.x ? 1 : -1);
-	yincr = (p1.y < p2.y ? 1 : -1);
-	x = p1.x;
-	y = p1.y;
-	err = dx / 2;
-	while (i < dx)
+	while (i < mlx->bres.dx)
 	{
-		x += xincr;
-		err += dy;
-		if (err > dx)
+		mlx->bres.x += mlx->bres.xincr;
+		mlx->bres.err += mlx->bres.dy;
+		if (mlx->bres.err > mlx->bres.dx)
 		{
-			err -= dx;
-			y += yincr;
+			mlx->bres.err -= mlx->bres.dx;
+			mlx->bres.y += mlx->bres.yincr;
 		}
-		fill_pixel(mlx->img.img_ui, x, y, p1.z);
+		fill_pixel(mlx->img.pixel_pos, mlx->bres.x, mlx->bres.y, p1.z);
 		i++;
 	}
-	fill_pixel(mlx->img.img_ui, p1.x, p1.y, p1.z);
-	fill_pixel(mlx->img.img_ui, p2.x, p2.y, p2.z);
 }
 
-int			draw_line(t_mlx *mlx, t_2dpoint p1, t_2dpoint p2)
+int				draw_line(t_mlx *mlx, t_2dpoint p1, t_2dpoint p2, int cam_mode)
 {
-	int dx;
-	int dy;
-	int dominant_axis;
+	int			dominant_axis;
+	t_2dpoint	temp_p1;
+	t_2dpoint	temp_p2;
 
-	dx = abs(p2.x - p1.x);
-	dy = abs(p2.y - p1.y);
-	dominant_axis = (dx > dy ? 'x' : 'y');
+	temp_p1 = p1;
+	temp_p2 = p2;
+	if (cam_mode == ISOMETRIC)
+	{
+		temp_p1.x = p1.iso_x;
+		temp_p1.y = p1.iso_y;
+		temp_p2.x = p2.iso_x;
+		temp_p2.y = p2.iso_y;
+	}
+	mlx->bres.dx = abs(temp_p2.x - temp_p1.x);
+	mlx->bres.dy = abs(temp_p2.y - temp_p1.y);
+	dominant_axis = (mlx->bres.dx > mlx->bres.dy ? 'x' : 'y');
 	if (dominant_axis == 'x')
-		draw_line_da_x(mlx, p1, p2);
+		draw_line_da_x(mlx, temp_p1, temp_p2);
 	else
-		draw_line_da_y(mlx, p1, p2);
-	return (0);
+		draw_line_da_y(mlx, temp_p1, temp_p2);
+	return (SUCCESS);
 }
 
-int			set_p_and_draw(t_mlx *mlx, t_map *map, int i, int j)
+int				set_point_and_draw(t_mlx *mlx, t_map *map, int i, int j)
 {
 	if (j == 0)
 	{
 		map->p[i][j] = set_point((900) - map->grid[i][j]
 				+ map->move_x,
 				(0) - map->grid[i][j]
-			   	+ (map->k * i) + map->move_y,
-				(map->grid[i][j]) * map->z_h);
-		iso(&map->p[i][j].x, &map->p[i][j].y, map->p[i][j].z);
+				+ (map->k * i) + map->move_y,
+				(map->grid[i][j]) * map->height_k);
 	}
 	else if (j > 0)
 	{
-		map->p[i][j] = set_point((900 - map->grid[i][0]) - map->grid[i][j]
+		map->p[i][j] = set_point((900 - map->grid[i][0])
+				- (map->grid[i][j] * map->height_k)
 				+ (map->k * j) + map->move_x,
-				((0) - map->grid[i][0]) - map->grid[i][j]
-			   	+ (map->k * i) + map->move_y,
-				(map->grid[i][j]) * map->z_h);
-		iso(&map->p[i][j].x, &map->p[i][j].y, map->p[i][j].z);
-		draw_line(mlx, map->p[i][j - 1], map->p[i][j]);
+				((0) - map->grid[i][0])
+				- (map->grid[i][j] * map->height_k)
+				+ (map->k * i) + map->move_y,
+				((map->grid[i][j]) * map->height_k));
+		draw_line(mlx, map->p[i][j - 1], map->p[i][j], map->cam);
 	}
 	if (i != 0)
-		draw_line(mlx, map->p[i - 1][j], map->p[i][j]);
-	return (0);
+		draw_line(mlx, map->p[i - 1][j], map->p[i][j], map->cam);
+	return (SUCCESS);
 }
 
-int			grid_setup(t_mlx *mlx, t_map *map)
+int				coordinates_setup(t_mlx *mlx, t_map *map)
 {
 	int i;
 	int j;
@@ -230,15 +201,15 @@ int			grid_setup(t_mlx *mlx, t_map *map)
 		j = 0;
 		while (j < map->len)
 		{
-			set_p_and_draw(mlx, map, i, j);
+			set_point_and_draw(mlx, map, i, j);
 			j++;
 		}
 		i++;
 	}
-	return (0);
+	return (SUCCESS);
 }
 
-int			grid_add_line(t_map *map, char *line, int line_nbr)
+int				grid_add_line(t_map *map, char *line, int line_nbr)
 {
 	char	**tmp;
 	int		i;
@@ -260,10 +231,10 @@ int			grid_add_line(t_map *map, char *line, int line_nbr)
 		free(*(tmp + i++));
 	}
 	free(tmp);
-	return (0);
+	return (SUCCESS);
 }
 
-int			line_count(char *map_name)
+int				line_count(char *map_name)
 {
 	int		lines;
 	char	*line;
@@ -278,7 +249,7 @@ int			line_count(char *map_name)
 	return (lines);
 }
 
-int			read_map(char *map_name, t_map *map)
+int				map_parse_to_int(char *map_name, t_map *map)
 {
 	int		fd;
 	int		line_nbr;
@@ -299,59 +270,100 @@ int			read_map(char *map_name, t_map *map)
 		line_nbr++;
 	}
 	close(fd);
-	return (0);
+	return (SUCCESS);
 }
 
-int			set_map(t_mlx *mlx, char *map_name)
+int				set_map(t_mlx *mlx, char *map_name)
 {
 	t_map	*map;
 
 	if (!(map = (t_map *)malloc(sizeof(t_map))) || !map_name || !(*map_name))
 		return (INVAL_MEM_ERROR);
-	map->k = SCALE(2); // distancia entre pontos e altura do z
-	map->z_h = 1;
+	map->k = SCALE(2);
+	map->height_k = 1;
+	map->cam = ISOMETRIC;
 	map->move_x = 0;
 	map->move_y = 0;
-	if (read_map(map_name, map) < 0)
+	if (map_parse_to_int(map_name, map) < 0)
 		return (INVAL_MAP_ERROR);
 	mlx->map = map;
 	if (!(map->p = (t_2dpoint **)malloc(sizeof(t_2dpoint *) * map->dep)))
 		return (INVAL_MEM_ERROR);
-	if (grid_setup(mlx, map) < 0)
+	if (coordinates_setup(mlx, map) < 0)
 		return (INVAL_MEM_ERROR);
 	mlx_put_image_to_window(mlx->mlx_ptr, mlx->win_ptr, mlx->img.img_ptr, 0, 0);
-	return (0);
+	return (SUCCESS);
 }
 
-int		image_refresh(t_mlx *mlx, char *mode)
+int				param_refresh(t_mlx *mlx, int mode)
+{
+	if (mode == ZOOM_IN)
+		mlx->map->k += 1;
+	else if (mode == ZOOM_OUT)
+		mlx->map->k -= (mlx->map->k == 1 ? 0 : 1);
+	else if (mode == INCREASE_HEIGHT)
+		mlx->map->height_k += 1;
+	else if (mode == DECREASE_HEIGHT)
+		mlx->map->height_k -= 1;
+	else if (mode == MOVE_TO_RIGHT)
+		mlx->map->move_x += 10;
+	else if (mode == MOVE_TO_LEFT)
+		mlx->map->move_x -= 10;
+	else if (mode == MOVE_UP)
+		mlx->map->move_y += 10;
+	else if (mode == MOVE_DOWN)
+		mlx->map->move_y -= 10;
+	else if (mode == ISOMETRIC)
+		mlx->map->cam = ISOMETRIC;
+	else if (mode == PARALLEL)
+		mlx->map->cam = PARALLEL;
+	else if (mode == EXIT)
+		exit(0);
+	return (SUCCESS);
+}
+
+int				initialize_menu(t_mlx *mlx)
+{
+	mlx_string_put(mlx->mlx_ptr, mlx->win_ptr, 90, 50, MENU_COLOR,
+			"___________________");
+	mlx_string_put(mlx->mlx_ptr, mlx->win_ptr, 90, 70, MENU_COLOR,
+			"|       MENU      |");
+	mlx_string_put(mlx->mlx_ptr, mlx->win_ptr, 90, 90, MENU_COLOR,
+			"| MOVE:  W A S D  |");
+	mlx_string_put(mlx->mlx_ptr, mlx->win_ptr, 90, 110, MENU_COLOR,
+			"| + ZOOM:     E   |");
+	mlx_string_put(mlx->mlx_ptr, mlx->win_ptr, 90, 130, MENU_COLOR,
+			"| - ZOOM:     Q   |");
+	mlx_string_put(mlx->mlx_ptr, mlx->win_ptr, 90, 150, MENU_COLOR,
+			"| + HEIGHT:   R   |");
+	mlx_string_put(mlx->mlx_ptr, mlx->win_ptr, 90, 170, MENU_COLOR,
+			"| + HEIGHT:   F   |");
+	mlx_string_put(mlx->mlx_ptr, mlx->win_ptr, 90, 190, MENU_COLOR,
+			"| ISOMETRIC:  Z   |");
+	mlx_string_put(mlx->mlx_ptr, mlx->win_ptr, 90, 210, MENU_COLOR,
+			"| PARALLEL:   X   |");
+	mlx_string_put(mlx->mlx_ptr, mlx->win_ptr, 90, 230, MENU_COLOR,
+			"| EXIT:      ESC  |");
+	mlx_string_put(mlx->mlx_ptr, mlx->win_ptr, 90, 245, MENU_COLOR,
+			"-------------------");
+	return (SUCCESS);
+}
+
+int				fdf_refresh(t_mlx *mlx, int mode)
 {
 	mlx_destroy_image(mlx->mlx_ptr, mlx->img.img_ptr);
-	if(!ft_strcmp(mode, "plus_scale"))
-		mlx->map->k += 1;
-	else if (!ft_strcmp(mode, "minus_scale"))
-		mlx->map->k -= (mlx->map->k == 1 ? 0 : 1);
-	else if (!ft_strcmp(mode, "plus_z_height"))
-		mlx->map->z_h += 1;
-	else if (!ft_strcmp(mode, "minus_z_height"))
-		mlx->map->z_h -= 1;
-	else if (!ft_strcmp(mode, "add_move_x"))
-		mlx->map->move_x += 10;
-	else if (!ft_strcmp(mode, "sub_move_x"))
-		mlx->map->move_x -= 10;
-	else if (!ft_strcmp(mode, "add_move_y"))
-		mlx->map->move_y += 10;
-	else if (!ft_strcmp(mode, "sub_move_y"))
-		mlx->map->move_y -= 10;
+	param_refresh(mlx, mode);
 	mlx->img.img_ptr = mlx_new_image(mlx->mlx_ptr, WIDTH, HEIGHT);
-	mlx->img.img_ui = (unsigned int *)mlx_get_data_addr(mlx->img.img_ptr,
+	mlx->img.pixel_pos = (unsigned int *)mlx_get_data_addr(mlx->img.img_ptr,
 			&(mlx->img.size_l), &(mlx->img.bpp), &(mlx->img.endian));
-	if (grid_setup(mlx, mlx->map) < 0)
+	if (coordinates_setup(mlx, mlx->map) < 0)
 		return (INVAL_MEM_ERROR);
 	mlx_put_image_to_window(mlx->mlx_ptr, mlx->win_ptr, mlx->img.img_ptr, 0, 0);
-	return (0);
+	initialize_menu(mlx);
+	return (SUCCESS);
 }
 
-int			fdf_start(char *map_name)
+int				fdf_start(char *map_name)
 {
 	t_mlx	*mlx;
 
@@ -360,18 +372,17 @@ int			fdf_start(char *map_name)
 	mlx->mlx_ptr = mlx_init();
 	mlx->win_ptr = mlx_new_window(mlx->mlx_ptr, WIDTH, HEIGHT, "Fdf");
 	mlx->img.img_ptr = mlx_new_image(mlx->mlx_ptr, WIDTH, HEIGHT);
-	mlx->img.img_ui = (unsigned int *)mlx_get_data_addr(mlx->img.img_ptr
+	mlx->img.pixel_pos = (unsigned int *)mlx_get_data_addr(mlx->img.img_ptr
 			, &(mlx->img.size_l), &(mlx->img.bpp), &(mlx->img.endian));
 	if (set_map(mlx, map_name) < 0)
 		return (INVAL_MAP_ERROR);
-	mlx_string_put(mlx->mlx_ptr, mlx->win_ptr,
-			400, 300, 0XFF00FF, "Belo chupa orla");
+	initialize_menu(mlx);
 	mlx_key_hook(mlx->win_ptr, deal_key, mlx);
 	mlx_loop(mlx->mlx_ptr);
-	return (1);
+	return (SUCCESS);
 }
 
-int			main(int argc, char **argv)
+int				main(int argc, char **argv)
 {
 	if (argc != 2)
 	{
